de6c476b042049ba1b51f1fa1c54bcec47e39307
jeky22 <qhzh33@gmail.com>
2025-12-12 12:33:06 +0900

    chore: Remove Drizzle ORM configuration and optional dependencies.

--- server/storage.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { MemStorage } from './storage';
-
-@Injectable()
-export class StorageService extends MemStorage {
-    constructor() {
-        super();
-    }
-}
